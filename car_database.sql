45e4cb6c1d22c6cc580aac3f269dfe1e09d491db
Sanjana Boddu <boddu.saa12@gmail.com>
2022-08-06 07:49:44 -0700

    Update schema.

--- car_database.sql
+++ car_database.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.0
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 04, 2022 at 09:06 AM
--- Server version: 10.1.25-MariaDB
--- PHP Version: 7.1.7
+-- Generation Time: Aug 06, 2022 at 04:49 PM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -29,20 +28,11 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `cars` (
-                        `id` int(11) NOT NULL,
-                        `name` varchar(255) NOT NULL,
-                        `brand` varchar(255) NOT NULL
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `brand` varchar(255) NOT NULL,
+  `user_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `cars`
---
-
-INSERT INTO `cars` (`id`, `name`, `brand`) VALUES
-                                               (1, 'maruti', 'suzuki'),
-                                               (4, 'CRV', 'honda'),
-                                               (5, 'corolla', 'toyota'),
-                                               (6, 'corolla', 'toyota');
 
 -- --------------------------------------------------------
 
@@ -51,18 +41,11 @@ INSERT INTO `cars` (`id`, `name`, `brand`) VALUES
 --
 
 CREATE TABLE `users` (
-                         `id` int(11) NOT NULL,
-                         `email` varchar(255) NOT NULL,
-                         `password` varchar(255) NOT NULL,
-                         `registered` varchar(255) NOT NULL
+  `id` int(11) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `registered` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`id`, `email`, `password`, `registered`) VALUES
-    (1, 'test@gmail.com', '$2b$10$l.O8wVZQBn/MRMYQw8GKruP24rf71LIHBtaGdCkrXU2GjxbQXhBzK', '1659596732');
 
 --
 -- Indexes for dumped tables
@@ -72,23 +55,31 @@ INSERT INTO `users` (`id`, `email`, `password`, `registered`) VALUES
 -- Indexes for table `cars`
 --
 ALTER TABLE `cars`
-    ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
 
 --
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
-    ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
+-- AUTO_INCREMENT for table `cars`
+--
+ALTER TABLE `cars`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-    MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;COMMIT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
